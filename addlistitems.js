7558b00813d062ec28d05611cc190c0fc88c230d
jessicapierre23 <jessica.pierre23@gmail.com>
2014-01-11 17:27:29 -0500

    commit new changes

--- addlistitems.js
+++ addlistitems.js
@@ -1,10 +1,26 @@
 //THE TOP LIST
+
+
+	
+$('input').keypress(function(e){
+    if(e.keyCode == 13){
+	    
+       e.preventDefault(); //with or without this line, it doesn't work
+	   return addItems();
+    }
+});
+
 function addItems() {
 
 var listform = document.getElementById("myList");
 var input = listform.elements[0].value; //input from text box
 
+if (/^\s+/.test(input)|| input == "") 
+   alert("Please enter an item and do not include spaces before the item.");
+   /*document.getElementById("livelist").innerHTML += "<p>Please enter an item</p>";*/
+else
 document.getElementById("livelist").innerHTML += "<li><input type='checkbox'/><label>" + input + "</label></li>";
+
 
 } //end addItems function
 
