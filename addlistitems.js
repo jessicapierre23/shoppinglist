5bc90ea405ca936104c2a507037faa9f1d5efe65
jessicapierre23 <jessica.pierre23@gmail.com>
2014-01-17 18:08:20 -0500

    added submit button

--- addlistitems.js
+++ addlistitems.js
@@ -1,5 +1,10 @@
-//THE TOP LIST
+
 $(document).ready(function() {
+
+$('form').submit(function(e){
+    e.preventDefault();
+	addItems();
+});
 
 $('input').keypress(function(e){
     if(e.keyCode == 13){
@@ -7,6 +12,8 @@ $('input').keypress(function(e){
         addItems();
     }
 });
+
+
 
 });
 
@@ -40,29 +47,3 @@ document.getElementById("livelist").innerHTML += "<li><input type='checkbox'/><l
 
 } //end addItems function
 
-/*THE BOTTOM LIST - USES ARRAY AND LOOPS
-function prelim() {    
-    secArray = ["avocado","apples","spinach"]; 
-    //don't use 'var' so variable can be public
-   for (item in secArray) {
-      document.getElementById("newdiv").innerHTML += "<li><input type='checkbox'/><label contenteditable='true'>" + secArray[item] + "</label></li>";
-	  /*it seems that adding the contenteditable attribute here does not work. why is that?*/
-  // } //end for loop
-// } //end prelim function
- 
-
-/*function testItems() {
-  var listform = document.getElementById("secList");
-  var secinput = listform.elements[0].value; //input from text box
-  
-  secArray.push(secinput);
-  
-  /*in the following lines here, what I originally tried to do was use a for loop to loop through all the array items, but the += would cause duplicate lists to appear in the div, and using = instead of += would cause the paragraph and previous item entries to not appear at all because it overwrites them. Is it better not to use loops at all with innerHTML statements?*/     
- /* document.getElementById("newdiv").innerHTML += "<input type='checkbox'/><label>" + secArray[secArray.length-1] + "</label><br/>";
-  
- 
-  
-      
-} *///end testItems function
-
-	
